a35d976f3ca04f5cb58b537f4b5dc5e60b833733
Poul <57594294+torconsult-dev@users.noreply.github.com>
2026-06-22 17:29:24 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,24 @@
-const CACHE='rson-eact-v08';
-const ASSETS=['./','./index.html','./Паспорт_объекта_app.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = 'rson-eact-v09';
+const ASSETS = [
+  './',
+  './index.html',
+  './Паспорт_объекта_app.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => null));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => null);
+    return resp;
+  }).catch(() => caches.match(event.request)));
+});
